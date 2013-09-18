a5c0f8423f80ae1ee18bafbe93ba02e1af48c94e
Andreas Sahlbach <andreas@sahlbach.com>
2013-09-18 16:38:51 +0200

    trying to fix classpath and web.xml locations

--- src/main/java/com/sahlbach/gradle/plugins/jetty9/internal/JettyConfiguration.java
+++ src/main/java/com/sahlbach/gradle/plugins/jetty9/internal/JettyConfiguration.java
@@ -17,37 +17,26 @@
 package com.sahlbach.gradle.plugins.jetty9.internal;
 
 import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.jetty.plus.annotation.InjectionCollection;
-import org.eclipse.jetty.plus.annotation.LifeCycleCallbackCollection;
-import org.eclipse.jetty.plus.annotation.RunAsCollection;
 import org.eclipse.jetty.plus.webapp.PlusConfiguration;
-import org.eclipse.jetty.servlet.FilterHolder;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.webapp.WebAppClassLoader;
-import org.eclipse.log.Log;
-import org.eclipse.util.LazyList;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 
 public class JettyConfiguration extends PlusConfiguration {
+    private static final Logger LOG = Log.getLogger(PlusConfiguration.class);
     private List<File> classPathFiles;
-    private File webXmlFile;
+    private File       webXmlFile;
 
-    public JettyConfiguration() {
+    public JettyConfiguration () {
         super();
     }
 
-    public void setClassPathConfiguration(List<File> classPathFiles) {
+    public void setClassPathConfiguration (List<File> classPathFiles) {
         this.classPathFiles = classPathFiles;
     }
 
-    public void setWebXml(File webXmlFile) {
+    public void setWebXml (File webXmlFile) {
         this.webXmlFile = webXmlFile;
     }
 
@@ -56,82 +45,80 @@ public class JettyConfiguration extends PlusConfiguration {
      *
      * @see org.eclipse.jetty.webapp.Configuration#configureClassLoader()
      */
-    public void configureClassLoader() throws Exception {
-        if (classPathFiles != null) {
-            Log.debug("Setting up classpath ...");
-
-            //put the classes dir and all dependencies into the classpath
-            for (File classPathFile : classPathFiles) {
-                ((WebAppClassLoader) getWebAppContext().getClassLoader()).addClassPath(
-                        classPathFile.getCanonicalPath());
-            }
-
-            if (Log.isDebugEnabled()) {
-                Log.debug("Classpath = " + LazyList.array2List(
-                        ((URLClassLoader) getWebAppContext().getClassLoader()).getURLs()));
-            }
-        } else {
-            super.configureClassLoader();
-        }
+    public void configureClassLoader () throws Exception {
+//        if (classPathFiles != null) {
+//            LOG.debug("Setting up classpath ...");
+//
+//            //put the classes dir and all dependencies into the classpath
+//            for (File classPathFile : classPathFiles) {
+//                ((WebAppClassLoader) getWebAppContext().getClassLoader()).addClassPath(classPathFile.getCanonicalPath());
+//            }
+//
+//            if (LOG.isDebugEnabled()) {
+//                LOG.debug("Classpath = " + LazyList.array2List(((URLClassLoader) getWebAppContext().getClassLoader()).getURLs()));
+//            }
+//        } else {
+//            super.configureClassLoader();
+//        }
     }
 
-    protected URL findWebXml() throws IOException {
-        //if an explicit web.xml file has been set (eg for jetty:run) then use it
-        if (webXmlFile != null && webXmlFile.exists()) {
-            return webXmlFile.toURI().toURL();
-        }
+//    protected URL findWebXml () throws IOException {
+//        //if an explicit web.xml file has been set (eg for jetty:run) then use it
+//        if (webXmlFile != null && webXmlFile.exists()) {
+//            return webXmlFile.toURI().toURL();
+//        }
+//
+//        //if we haven't overridden location of web.xml file, use the
+//        //standard way of finding it
+//        LOG.debug("Looking for web.xml file in WEB-INF");
+//        return super.findWebXml();
+//    }
 
-        //if we haven't overridden location of web.xml file, use the
-        //standard way of finding it
-        Log.debug("Looking for web.xml file in WEB-INF");
-        return super.findWebXml();
-    }
-
-    public void parseAnnotations() throws Exception {
-        String v = System.getProperty("java.version");
-        String[] version = v.split("\\.");
-        if (version == null) {
-            Log.info("Unable to determine jvm version, annotations will not be supported");
-            return;
-        }
-        int major = Integer.parseInt(version[0]);
-        int minor = Integer.parseInt(version[1]);
-        if ((major >= 1) && (minor >= 5)) {
-            //TODO it would be nice to be able to re-use the parseAnnotations() method on
-            //the org.eclipse.jetty.annotations.Configuration class, but it's too difficult?
-
-            //able to use annotations on on jdk1.5 and above
-            Class<?> annotationParserClass = Thread.currentThread().getContextClassLoader().loadClass(
-                    "org.eclipse.jetty.annotations.AnnotationParser");
-            Method parseAnnotationsMethod = annotationParserClass.getMethod("parseAnnotations", WebAppContext.class,
-                    Class.class, RunAsCollection.class, InjectionCollection.class, LifeCycleCallbackCollection.class);
-
-            //look thru _servlets
-            Iterator itor = LazyList.iterator(_servlets);
-            while (itor.hasNext()) {
-                ServletHolder holder = (ServletHolder) itor.next();
-                Class servlet = getWebAppContext().loadClass(holder.getClassName());
-                parseAnnotationsMethod.invoke(null, getWebAppContext(), servlet, _runAsCollection, _injections,
-                        _callbacks);
-            }
-
-            //look thru _filters
-            itor = LazyList.iterator(_filters);
-            while (itor.hasNext()) {
-                FilterHolder holder = (FilterHolder) itor.next();
-                Class filter = getWebAppContext().loadClass(holder.getClassName());
-                parseAnnotationsMethod.invoke(null, getWebAppContext(), filter, null, _injections, _callbacks);
-            }
-
-            //look thru _listeners
-            itor = LazyList.iterator(_listeners);
-            while (itor.hasNext()) {
-                Object listener = itor.next();
-                parseAnnotationsMethod.invoke(null, getWebAppContext(), listener.getClass(), null, _injections,
-                        _callbacks);
-            }
-        } else {
-            Log.info("Annotations are not supported on jvms prior to jdk1.5");
-        }
-    }
+//    public void parseAnnotations() throws Exception {
+//        String v = System.getProperty("java.version");
+//        String[] version = v.split("\\.");
+//        if (version == null) {
+//            LOG.info("Unable to determine jvm version, annotations will not be supported");
+//            return;
+//        }
+//        int major = Integer.parseInt(version[0]);
+//        int minor = Integer.parseInt(version[1]);
+//        if ((major >= 1) && (minor >= 5)) {
+//            //TODO it would be nice to be able to re-use the parseAnnotations() method on
+//            //the org.eclipse.jetty.annotations.Configuration class, but it's too difficult?
+//
+//            //able to use annotations on on jdk1.5 and above
+//            Class<?> annotationParserClass = Thread.currentThread().getContextClassLoader().loadClass(
+//                    "org.eclipse.jetty.annotations.AnnotationParser");
+//            Method parseAnnotationsMethod = annotationParserClass.getMethod("parseAnnotations", WebAppContext.class,
+//                    Class.class, RunAsCollection.class, InjectionCollection.class, LifeCycleCallbackCollection.class);
+//
+//            //look thru _servlets
+//            Iterator itor = LazyList.iterator(_servlets);
+//            while (itor.hasNext()) {
+//                ServletHolder holder = (ServletHolder) itor.next();
+//                Class servlet = getWebAppContext().loadClass(holder.getClassName());
+//                parseAnnotationsMethod.invoke(null, getWebAppContext(), servlet, _runAsCollection, _injections,
+//                        _callbacks);
+//            }
+//
+//            //look thru _filters
+//            itor = LazyList.iterator(_filters);
+//            while (itor.hasNext()) {
+//                FilterHolder holder = (FilterHolder) itor.next();
+//                Class filter = getWebAppContext().loadClass(holder.getClassName());
+//                parseAnnotationsMethod.invoke(null, getWebAppContext(), filter, null, _injections, _callbacks);
+//            }
+//
+//            //look thru _listeners
+//            itor = LazyList.iterator(_listeners);
+//            while (itor.hasNext()) {
+//                Object listener = itor.next();
+//                parseAnnotationsMethod.invoke(null, getWebAppContext(), listener.getClass(), null, _injections,
+//                        _callbacks);
+//            }
+//        } else {
+//            LOG.info("Annotations are not supported on jvms prior to jdk1.5");
+//        }
+//    }
 }
--- src/main/java/com/sahlbach/gradle/plugins/jetty9/internal/JettyPluginWebAppContext.java
+++ src/main/java/com/sahlbach/gradle/plugins/jetty9/internal/JettyPluginWebAppContext.java
@@ -17,64 +17,68 @@
 package com.sahlbach.gradle.plugins.jetty9.internal;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.eclipse.jetty.plus.webapp.EnvConfiguration;
-import org.eclipse.jetty.webapp.Configuration;
-import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
-import org.eclipse.jetty.webapp.TagLibConfiguration;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.*;
 
 /**
  * Jetty9PluginWebAppContext
  */
 public class JettyPluginWebAppContext extends WebAppContext {
     private List<File> classpathFiles;
-    private File jettyEnvXmlFile;
-    private File webXmlFile;
-    private WebInfConfiguration webInfConfig = new WebInfConfiguration();
-    private EnvConfiguration envConfig = new EnvConfiguration();
-    private JettyConfiguration mvnConfig = new JettyConfiguration();
-    private JettyWebXmlConfiguration jettyWebConfig = new JettyWebXmlConfiguration();
-    private TagLibConfiguration tagConfig = new TagLibConfiguration();
-    private Configuration[] configs = new Configuration[]{
-            webInfConfig, envConfig, mvnConfig, jettyWebConfig, tagConfig
-    };
+    private File       jettyEnvXmlFile;
+    private File       webXmlFile;
+    private WebXmlConfiguration      webXmlConfiguration = new WebXmlConfiguration();
+    private WebInfConfiguration      webInfConfig        = new WebInfConfiguration();
+    private EnvConfiguration         envConfig           = new EnvConfiguration();
+    private JettyConfiguration       jettyConfiguration  = new JettyConfiguration();
+    private JettyWebXmlConfiguration jettyWebConfig      = new JettyWebXmlConfiguration();
+    private TagLibConfiguration      tagConfig           = new TagLibConfiguration();
+    private Configuration[]          configs             = new Configuration[] {webXmlConfiguration,
+                                                                                webInfConfig,
+                                                                                envConfig,
+                                                                                jettyConfiguration,
+                                                                                jettyWebConfig,
+                                                                                tagConfig};
 
-    public JettyPluginWebAppContext() {
+    public JettyPluginWebAppContext () {
         super();
         setConfigurations(configs);
     }
 
-    public void setClassPathFiles(List<File> classpathFiles) {
+    public void setClassPathFiles (List<File> classpathFiles) {
         this.classpathFiles = classpathFiles;
     }
 
-    public List<File> getClassPathFiles() {
+    public List<File> getClassPathFiles () {
         return this.classpathFiles;
     }
 
-    public void setWebXmlFile(File webXmlFile) {
+    public void setWebXmlFile (File webXmlFile) {
         this.webXmlFile = webXmlFile;
     }
 
-    public File getWebXmlFile() {
+    public File getWebXmlFile () {
         return this.webXmlFile;
     }
 
-    public void setJettyEnvXmlFile(File jettyEnvXmlFile) {
+    public void setJettyEnvXmlFile (File jettyEnvXmlFile) {
         this.jettyEnvXmlFile = jettyEnvXmlFile;
     }
 
-    public File getJettyEnvXmlFile() {
+    public File getJettyEnvXmlFile () {
         return this.jettyEnvXmlFile;
     }
 
-    public void configure() {
+    public void configure () throws IOException {
+        if (webXmlFile != null)
+            setDescriptor(webXmlFile.getCanonicalPath());
         setConfigurations(configs);
-        mvnConfig.setClassPathConfiguration(classpathFiles);
-        mvnConfig.setWebXml(webXmlFile);
+
+        jettyConfiguration.setClassPathConfiguration(classpathFiles);
+        //        jettyConfiguration.setWebXml(webXmlFile);
         try {
             if (this.jettyEnvXmlFile != null) {
                 envConfig.setJettyEnvXml(this.jettyEnvXmlFile.toURI().toURL());
@@ -82,29 +86,6 @@ public class JettyPluginWebAppContext extends WebAppContext {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        /*
-        Configuration[] configurations = getConfigurations();
-        for (int i=0;i<configurations.length; i++)
-        {
-            if (configurations[i] instanceof JettyConfiguration)
-            {
-                ((JettyConfiguration)configurations[i]).setClassPathConfiguration (classpathFiles);
-                ((JettyConfiguration)configurations[i]).setWebXml (webXmlFile);
-            }
-            else if (configurations[i] instanceof EnvConfiguration)
-            {
-                try
-                {
-                    if (this.jettyEnvXmlFile != null)
-                        ((EnvConfiguration)configurations[i]).setJettyEnvXml(this.jettyEnvXmlFile.toURL());
-                }
-                catch (Exception e)
-                {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-        */
     }
 
     public void doStart() throws Exception {
