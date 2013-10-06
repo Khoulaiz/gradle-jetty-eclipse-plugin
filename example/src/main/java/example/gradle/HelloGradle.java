90f6b261d4eee7d24ba7a882e9121abb434cfe39
Andreas Sahlbach <andreas@sahlbach.com>
2013-10-06 02:28:47 +0200

    added example project

--- example/src/main/java/example/gradle/HelloGradle.java
+++ example/src/main/java/example/gradle/HelloGradle.java
@@ -1,16 +1,31 @@
+/*
+ * Copyright 2012-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package example.gradle;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.ServletConfig;
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = {"/HelloGradle"}, name="HelloGradle")
 @ServletSecurity(@HttpConstraint(rolesAllowed={"hacker"}))
